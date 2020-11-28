62ea52d0b0251387f096e3d8eb74e38e645fdd15
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2020-11-29 00:18:14 +0400

    open payment

---
+++ mis_open_payment/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mis Open Payment',
+    'version': '13.0.1',
+    'category': 'Mis Open Payment',
+    'summary': """ """,
+    'description': """ Open Payment
+                    """,
+    'author': "Hafeel",
+    'website': 'Mindifnosys.com',
+    'depends': ['base', 'account'],
+    'data': [
+        'views/openpayment_views.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
