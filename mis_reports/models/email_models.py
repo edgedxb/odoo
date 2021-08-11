38e3df5d6f1969fd2acfa1f523a9d7fb81f468cd
mindinfosys <67480042+mindinfosys@users.noreply.github.com>
2021-08-11 23:57:00 +0400

    update

--- mis_reports/models/email_models.py
+++ mis_reports/models/email_models.py
@@ -10,10 +10,16 @@ from odoo.exceptions import UserError, ValidationError
 class MisCRMLead(models.Model):
     _name = 'notpaid.invoices'
 
-    def _send_notpaid_email_notification(self):
+    def _get_notpaid_lines(self):
         objinvoice = self.env['account.move'].search([('state', '=', 'posted'),
                                                       ('move_type', 'in', ('out_invoice', 'out_refund')),
                                                       ('company_id', '=', 1),
                                                       ('amount_residual_signed', '>', 0.0)])
         return objinvoice
+
+    def _send_notpaid_email_notification(self):
+        email_template = self.env.ref('mis_reports.email_template_notpaid_invoice')
+
+        if email_template:
+            email_template.send_mail(force_send=True)
 
