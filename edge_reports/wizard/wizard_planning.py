0c95befcee71562569f31b047a3362895ea7068b
Hafeel <hafeel@asdubai.org>
2021-11-08 09:38:33 +0400

    update

--- edge_reports/wizard/wizard_planning.py
+++ edge_reports/wizard/wizard_planning.py
@@ -81,40 +81,11 @@ class PlanningSummary(models.TransientModel):
         column_width=25
         worksheet.set_column(col, col, column_width)
         worksheet.write(count, col, 'Time', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 1', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 2', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 3', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 4', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 5', wbf['content_border_bg'])
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 6', wbf['content_border_bg'])
-
-        col += 1
-        column_width = 40
-        worksheet.set_column(col, col, column_width)
-        worksheet.write(count, col, 'Team 7', wbf['content_border_bg'])
 
         tmr = 8.0
 
-        filterstartdate =self.current_date
-        filterenddate =filterstartdate+timedelta(days=1)
+        filterstartdate = self.current_date
+        filterenddate = filterstartdate + timedelta(days=1)
 
         for tmcount in range(25):
             strtime = str(
@@ -123,323 +94,61 @@ class PlanningSummary(models.TransientModel):
             worksheet.set_row(count, 30)
             worksheet.write(count, 0, strtime, wbf['content_border_bg'])
             tmr += 0.5
-#Team 1====================================
-        objteam1 = self.env['planning.slot'].search([('role_id.name','=', 'Team1'), ('start_datetime','>=', filterstartdate), ('end_datetime','<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
 
-        for recteam1 in objteam1:
-            startminutes_val=(recteam1.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val/30)
 
-            endminutes_val = (recteam1.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
+        objrole = self.env['planning.role'].search([], order='id')
+        for rec_role in objrole:
+            count = 1
+            col += 1
+            column_width = 40
+            worksheet.set_column(col, col, column_width)
+            worksheet.write(count, col, rec_role.name, wbf['content_border_bg'])
 
-            endrow = int(endminutes_val / 30)
-            #raise UserError(endrow)
-            if  recteam1.crm_id:
-                strprint =''
-                if recteam1.partner_id:
-                    strprint+=recteam1.partner_id.name
-                    strprint+='\n'
-                if recteam1.phone:
-                    strprint+=recteam1.phone
-                    strprint+='\n'
-                if recteam1.unit_number:
-                    strprint+=recteam1.unit_number
-                    strprint+='\n'
-                if recteam1.building_name:
-                    strprint+=recteam1.building_name
-                    strprint+='\n'
-                if recteam1.area_id:
-                    strprint+=recteam1.area_id.name
-                    strprint+='\n'
-                if recteam1.scope:
-                    strprint+=recteam1.scope
-                    strprint+='\n'
-                if recteam1.revenue:
-                    strprint +='AED - '
-                    strprint+=str('{0:.2f}'.format(recteam1.revenue))
-                    strprint+='\n'
-                #count += 1
-                worksheet.merge_range('B%s:B%s' % (count+startrow, count+endrow-1), strprint, wbf['content_border_bg'])
 
-# Team 2====================================
+            # Team data ====================================
+            objteam1 = self.env['planning.slot'].search(
+                [('role_id.name', '=', rec_role.name), ('start_datetime', '>=', filterstartdate),
+                 ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
+            # col = 1
+            count = 2
 
-        objteam2 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team2'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
+            for recteam1 in objteam1:
+                startminutes_val = (recteam1.start_datetime)
+                startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
+                startrow = int(startminutes_val / 30)
 
-        for recteam2 in objteam2:
-            startminutes_val = (recteam2.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
+                endminutes_val = (recteam1.end_datetime)
+                endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
 
-            endminutes_val = (recteam2.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
+                endrow = int(endminutes_val / 30)
 
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam2.crm_id:
-                strprint = ''
-                if recteam2.partner_id:
-                    strprint += recteam2.partner_id.name
-                    strprint += '\n'
-                if recteam2.phone:
-                    strprint += recteam2.phone
-                    strprint += '\n'
-                if recteam2.unit_number:
-                    strprint += recteam2.unit_number
-                    strprint += '\n'
-                if recteam2.building_name:
-                    strprint += recteam2.building_name
-                    strprint += '\n'
-                if recteam2.area_id:
-                    strprint += recteam2.area_id.name
-                    strprint += '\n'
-                if recteam2.scope:
-                    strprint += recteam2.scope
-                    strprint += '\n'
-                if recteam2.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam2.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('C%s:C%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
-
-# Team 3====================================
-
-        objteam3 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team3'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
-
-        for recteam3 in objteam3:
-            startminutes_val = (recteam3.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
-
-            endminutes_val = (recteam3.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
-
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam3.crm_id:
-                strprint = ''
-                if recteam3.partner_id:
-                    strprint += recteam3.partner_id.name
-                    strprint += '\n'
-                if recteam3.phone:
-                    strprint += recteam3.phone
-                    strprint += '\n'
-                if recteam3.unit_number:
-                    strprint += recteam3.unit_number
-                    strprint += '\n'
-                if recteam3.building_name:
-                    strprint += recteam3.building_name
-                    strprint += '\n'
-                if recteam3.area_id:
-                    strprint += recteam3.area_id.name
-                    strprint += '\n'
-                if recteam3.scope:
-                    strprint += recteam3.scope
-                    strprint += '\n'
-                if recteam3.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam3.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('D%s:D%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
-
-# Team 4====================================
-
-        objteam4 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team4'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
-
-        for recteam4 in objteam4:
-            startminutes_val = (recteam4.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
-
-            endminutes_val = (recteam4.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
-
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam4.crm_id:
-                strprint = ''
-                if recteam4.partner_id:
-                    strprint += recteam4.partner_id.name
-                    strprint += '\n'
-                if recteam4.phone:
-                    strprint += recteam4.phone
-                    strprint += '\n'
-                if recteam4.unit_number:
-                    strprint += recteam4.unit_number
-                    strprint += '\n'
-                if recteam4.building_name:
-                    strprint += recteam4.building_name
-                    strprint += '\n'
-                if recteam4.area_id:
-                    strprint += recteam4.area_id.name
-                    strprint += '\n'
-                if recteam4.scope:
-                    strprint += recteam4.scope
-                    strprint += '\n'
-                if recteam4.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam4.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('E%s:E%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
-
-# Team 5====================================
-
-        objteam5 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team5'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
-
-        for recteam5 in objteam5:
-            startminutes_val = (recteam5.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
-
-            endminutes_val = (recteam5.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
-
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam5.crm_id:
-                strprint = ''
-                if recteam5.partner_id:
-                    strprint += recteam5.partner_id.name
-                    strprint += '\n'
-                if recteam5.phone:
-                    strprint += recteam5.phone
-                    strprint += '\n'
-                if recteam5.unit_number:
-                    strprint += recteam5.unit_number
-                    strprint += '\n'
-                if recteam5.building_name:
-                    strprint += recteam5.building_name
-                    strprint += '\n'
-                if recteam5.area_id:
-                    strprint += recteam5.area_id.name
-                    strprint += '\n'
-                if recteam5.scope:
-                    strprint += recteam5.scope
-                    strprint += '\n'
-                if recteam5.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam5.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('F%s:F%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
-
-# Team 6====================================
-
-        objteam6 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team6'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
-
-        for recteam6 in objteam6:
-            startminutes_val = (recteam6.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
-
-            endminutes_val = (recteam6.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
-
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam6.crm_id:
-                strprint = ''
-                if recteam6.partner_id:
-                    strprint += recteam6.partner_id.name
-                    strprint += '\n'
-                if recteam6.phone:
-                    strprint += recteam6.phone
-                    strprint += '\n'
-                if recteam6.unit_number:
-                    strprint += recteam6.unit_number
-                    strprint += '\n'
-                if recteam6.building_name:
-                    strprint += recteam6.building_name
-                    strprint += '\n'
-                if recteam6.area_id:
-                    strprint += recteam6.area_id.name
-                    strprint += '\n'
-                if recteam6.scope:
-                    strprint += recteam6.scope
-                    strprint += '\n'
-                if recteam6.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam6.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('G%s:G%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
-
-# Team 7====================================
-
-        objteam7 = self.env['planning.slot'].search(
-            [('role_id.name', '=', 'Team7'), ('start_datetime', '>=', filterstartdate),
-             ('end_datetime', '<', filterenddate)], order='start_datetime,end_datetime')
-        col = 1
-        count = 3
-
-        for recteam7 in objteam7:
-            startminutes_val = (recteam7.start_datetime)
-            startminutes_val = (startminutes_val.minute + ((startminutes_val.hour + 4) * 60) - 480)
-            startrow = int(startminutes_val / 30)
-
-            endminutes_val = (recteam7.end_datetime)
-            endminutes_val = (endminutes_val.minute + ((endminutes_val.hour + 4) * 60) - 480)
-
-            endrow = int(endminutes_val / 30)
-            # raise UserError(endrow)
-            if recteam7.crm_id:
-                strprint = ''
-                if recteam7.partner_id:
-                    strprint += recteam7.partner_id.name
-                    strprint += '\n'
-                if recteam7.phone:
-                    strprint += recteam7.phone
-                    strprint += '\n'
-                if recteam7.unit_number:
-                    strprint += recteam7.unit_number
-                    strprint += '\n'
-                if recteam7.building_name:
-                    strprint += recteam7.building_name
-                    strprint += '\n'
-                if recteam7.area_id:
-                    strprint += recteam7.area_id.name
-                    strprint += '\n'
-                if recteam7.scope:
-                    strprint += recteam7.scope
-                    strprint += '\n'
-                if recteam7.revenue:
-                    strprint += 'AED - '
-                    strprint += str('{0:.2f}'.format(recteam7.revenue))
-                    strprint += '\n'
-                # count += 1
-                worksheet.merge_range('H%s:H%s' % (count + startrow, count + endrow - 1), strprint,
-                                      wbf['content_border_bg'])
+                if recteam1.crm_id:
+                    strprint = ''
+                    if recteam1.partner_id:
+                        strprint += recteam1.partner_id.name
+                        strprint += '\n'
+                    if recteam1.phone:
+                        strprint += recteam1.phone
+                        strprint += '\n'
+                    if recteam1.unit_number:
+                        strprint += recteam1.unit_number
+                        strprint += '\n'
+                    if recteam1.building_name:
+                        strprint += recteam1.building_name
+                        strprint += '\n'
+                    if recteam1.area_id:
+                        strprint += recteam1.area_id.name
+                        strprint += '\n'
+                    if recteam1.scope:
+                        strprint += recteam1.scope
+                        strprint += '\n'
+                    if recteam1.revenue:
+                        strprint += 'AED - '
+                        strprint += str('{0:.2f}'.format(recteam1.revenue))
+                        strprint += '\n'
+                    # count += 1
+                    worksheet.merge_range(str(chr(65+col))+str(count + startrow)+':'+str(chr(65+col))+str(count + endrow - 1), strprint,
+                                          wbf['content_border_bg'])
 
 
         workbook.close()
