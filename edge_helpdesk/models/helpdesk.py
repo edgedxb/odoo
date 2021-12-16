63e665261064d3c96b6f8bf305630dbd8d63d353
Hafeel <hafeel@asdubai.org>
2021-12-17 00:25:18 +0400

    update

---
+++ edge_helpdesk/models/helpdesk.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+from werkzeug.urls import url_encode
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    company_id = fields.Many2one('res.company', 'Company', required=True, index=True,
+                                 default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
+    amount_total = fields.Monetary(string='Total', related="sale_order_id.amount_total")
+    user_id = fields.Many2one('res.users', string='Salesperson', related="sale_order_id.user_id")
