e8b1ac18ff73b8fc8bda0b5d4ecb3d3e39592442
Hafeel <hafeel@asdubai.org>
2021-12-16 23:43:36 +0400

    edge

--- edge_helpdesk/models/helpdesk.py
+++ edge_helpdesk/models/helpdesk.py
@@ -7,5 +7,5 @@ from odoo import api, fields, models
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    amount_total = fields.Monetary(string='Total', related="sale_order_id.amount_total")
+    amount_total = fields.Float(string='Total', related="sale_order_id.amount_total")
     user_id = fields.Many2one('res.users', string='Salesperson', related="sale_order_id.user_id")
